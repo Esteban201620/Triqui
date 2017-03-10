d9442888f68a0b75986eb88ad00a809ae158b66b
Estudiantes <Estudiantes@labing50206.udistrital.edu.co>
2017-03-10 17:08:34 -0500

    Commit 1

---
+++ JuegoTriqui/src/juegotriqui/Jugador.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegotriqui;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Jugador {
+    String Jugador1;
+    String Jugador2;
+    
+    
+    public void PonerFicha(){
+        
+    }
+}
---
+++ JuegoTriqui/src/juegotriqui/Tablero.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegotriqui;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Tablero {
+    char Ficha[]= new char[9];
+    //if ()
+}
